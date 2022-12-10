c6e0b63e3cf1b78291f050b7b68fedc6758e73ab
jorge <jorge.ricard@utp.edu.co>
2022-12-10 12:50:41 -0500

    primer commit

---
+++ PokedexMVVM/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using PokedexMVVM.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PokedexMVVM
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage (new Registrarpokemon());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ PokedexMVVM/Models/Mpokemon.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PokedexMVVM.Models
+{
+    public class Mpokemon
+    {
+        public string Colorfondo { get; set; }
+        public string Icono { get; set; }
+        public string Nombre { get; set; }
+        public string Nroorden { get; set; }
+        public string Poder { get; set; }
+        public string Idpokemon { get; set; }
+    }
+}
